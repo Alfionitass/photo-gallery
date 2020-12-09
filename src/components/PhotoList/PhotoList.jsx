3f10feca995047fdcc65b513e8474d513b96cc92
Alfionitass <alfionitasaadah@gmail.com>
2020-12-09 16:59:52 +0700

    get photo data

--- src/components/PhotoList/PhotoList.jsx
+++ src/components/PhotoList/PhotoList.jsx
@@ -2,99 +2,44 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import fetchJsonp from 'fetch-jsonp';
 import { Container, Grid, Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography } from "@material-ui/core";
-import rs from "../../assets/rs.png"
+import nf from "../../assets/unnamed.png"
 import styles from "./PhotoList.module.css";
 
 export default function PhotoList() {
     const [photos, setPhotos] = useState();
 
     useEffect(() => {
-        fetchJsonp('https://api.flickr.com/services/feeds/photos_public.gne', {
+        axios({
             method: 'GET',
-            mode: 'no-cors',
-            credentials: 'include',
-            jsonpCallback: 'photo_results'
+            url: "https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=e8a7f5fd9748f2567214440d10611b4a&format=json&nojsoncallback=true",
         })
-            .then(function (response) {
-                return response.json()
-            }).then(function (json) {
-                console.log('data poto', json)
-            }).catch(function (ex) {
-                console.log('parsing failed', ex)
-            })
+        .then(res => {
+            if (res.status === 200) {
+                //console.log("data", res)
+                setPhotos(res?.data?.photos)
+            }
+        })
+        .catch(err => {
+            console.log("errornya", err)
+        })
     }, [])
 
     return (
-        <Grid container spacing={5} className={styles.container}>
-            <Grid item md={4} sm={6} xs={12} >
-                <Card className={styles.card}>
-                    <CardActionArea>
-                        <CardMedia
-                            className={styles.media}
-                            component="img"
-                            image={rs}
-                            title="Hospital"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                Malang Hospital
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-            </Grid>
-            <Grid item md={4} sm={6} xs={12} spacing={2}>
-                <Card className={styles.card}>
-                    <CardActionArea>
-                        <CardMedia
-                            className={styles.media}
-                            component="img"
-                            image={rs}
-                            title="Hospital"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                Malang Hospital
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-            </Grid>
-            <Grid item md={4} sm={6} xs={12} spacing={2}>
-                <Card className={styles.card}>
-                    <CardActionArea>
-                        <CardMedia
-                            className={styles.media}
-                            component="img"
-                            image={rs}
-                            title="Hospital"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                Malang Hospital
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-            </Grid>
-            <Grid item md={4} sm={6} xs={12} spacing={2}>
-                <Card className={styles.card}>
-                    <CardActionArea>
-                        <CardMedia
-                            className={styles.media}
-                            component="img"
-                            image={rs}
-                            title="Hospital"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                Malang Hospital
-                            </Typography>
-                        </CardContent>
-                    </CardActionArea>
-                </Card>
-            </Grid>
-
+        <Grid container spacing={3} className={styles.container}>
+            {photos?.photo.map((photo, i) => (
+                <Grid item md={4} sm={6} xs={12} key={i} >
+                    <Card className={styles.card}>
+                        <CardActionArea>
+                            <CardMedia
+                                className={styles.cardMedia}
+                                component="img"
+                                image= {`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+                                title= {photo.title}
+                            />
+                        </CardActionArea>
+                    </Card>
+                </Grid>
+            ))}
         </Grid>
     )
 }
